a0609b45f29820dbde885cb4d5457d523ccd0fa1
Sergey Bolshchikov <sergey@bolshchikov.net>
2021-01-16 16:49:08 +0200

    make key valid

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
       if (!data) {
         return;
       }
-      this.key = `${data.boardId}+${data.itemId}`;
+      this.key = `${data.boardId}:${data.itemId}`;
     }).then(() => this.readData());
   }
 
